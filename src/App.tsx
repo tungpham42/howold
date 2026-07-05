85629c7ad560ae54bbea4410a31ada11506d30e8
Tung Pham <tung.42@gmail.com>
2026-07-05 11:34:27 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -157,6 +157,12 @@ const App: React.FC = () => {
         const MODEL_URL = process.env.PUBLIC_URL + "/models";
         await Promise.all([
           faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+          faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
+          faceapi.nets.mtcnn.loadFromUri(MODEL_URL),
+          faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+          faceapi.nets.faceLandmark68TinyNet.loadFromUri(MODEL_URL),
+          faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+          faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
           faceapi.nets.ageGenderNet.loadFromUri(MODEL_URL),
         ]);
         setModelsLoaded(true);
